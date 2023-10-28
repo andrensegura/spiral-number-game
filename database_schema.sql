65027742bf75efe5270157a45e002cda2fd81761
Andre Segura <andrensegura@gmail.com>
2023-10-28 13:08:51 -0700

    db table 'items' new column 'tags'

--- database_schema.sql
+++ database_schema.sql
@@ -3,17 +3,15 @@ CREATE TABLE players(
    suds type integer,
    bonus type integer,
    penalty type integer,
-   inventory type text,
+   inventory type text, score_7d number, dailysuds number, safemode int default 1 not null, safemode_timer TEXT default "2023-05-10 23:59:59" not null,
    UNIQUE (username)
 );
 CREATE TABLE items (
    primary_key INTEGER PRIMARY KEY,
    name type text,
    price type integer,
-   description type text,
-   stock type integer default 0,
-   forsale type integer default 0
-);
+   description type text
+, stock type integer default 0, forsale type integer default 0, unlimited integer default 0 not null, tags text);
 CREATE TABLE submissions (
    id type text,
    UNIQUE (id)
@@ -23,8 +21,8 @@ CREATE TABLE comments (
    UNIQUE (id)
 );
 CREATE TABLE stats (
-   name text primary key,
-   lifetime integer default 0,
-   week integer default 0,
-   day integer default 0
+name text primary key,
+lifetime integer default 0,
+week integer default 0,
+day integer default 0
 );
